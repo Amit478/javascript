991448b0067efdfab781d40b9906cdb552458339
Amit Wagadre <100210355+Amit478@users.noreply.github.com>
2023-11-05 20:02:22 +0000

    variable config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 1111
+let accountEmail ="amit@yahoo.com"
+var accountPassword = "0000"
+accountCity = "Indore"
+
+accountEmail = "ritik@google.com"
+accountPassword = "9999"
+accountCity ="Bhopal"
+
+console.table([ accountId, accountEmail, accountPassword, accountCity]);
